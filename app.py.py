3b20b395808e1761e8f798afb7cc3b58e2827b45
fkuontay82 <fkuontayp@gmail.com>
2026-05-16 09:59:21 -0500

    Update app.py.py

--- app.py.py
+++ app.py.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import libreria.funciones as lf
 
 st.title("Proyecto final UCG")
 st.sidebar.title("Parámetros")
@@ -9,3 +11,5 @@ uploaded_files = st.file_uploader(
 for uploaded_file in uploaded_files:
     df = pd.read_csv(uploaded_file)
     st.write(df)
+cuota = lf.cuota_prestamo(1000, 0.10,2,12)
+st.write(cuota)
