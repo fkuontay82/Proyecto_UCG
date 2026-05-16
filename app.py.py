a8855f2d02c69f11b761ae3467f51aba6d549ae7
fkuontay82 <fkuontayp@gmail.com>
2026-05-16 09:54:54 -0500

    Update app.py.py

--- app.py.py
+++ app.py.py
@@ -3,3 +3,9 @@ import streamlit as st
 st.title("Proyecto final UCG")
 st.sidebar.title("Parámetros")
 st.sidebar.image("Logo Python2.png")
+uploaded_files = st.file_uploader(
+    "Upload data", accept_multiple_files=True, type="csv"
+)
+for uploaded_file in uploaded_files:
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
