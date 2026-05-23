d938abba12e89cc0e036930a0aac7f6f9d9fce19
fkuontay82 <fkuontayp@gmail.com>
2026-05-23 10:16:06 -0500

    Add Streamlit setup and sidebar configuration

--- app.py.py
+++ app.py.py
@@ -1,3 +1,14 @@
+import streamlit as st
+import pandas as pd
+import libreria_funciones as lf
+import librería_clases as lc
+
+st.title("Proyecto final UCG")
+
+st.sidebar.title("Parámetros")
+
+st.sidebar.image("Logo Python2.png")
+
 PAGES = {
     "Modulo 1: Carga de información": "modulo1",
     "Modulo 2: Registro de: interes, monto, cuotas": "modulo2",
